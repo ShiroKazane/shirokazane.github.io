f7e9aeda168a3ef8996215bb2718fc4f5204e631
ShiroKazane <bayuofficial773@gmail.com>
2022-09-03 16:54:50 +0700

    modified:   assets/css/main.css 	modified:   assets/js/preloader.js 	modified:   index.html

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     window.setTimeout(function() {
         document.querySelector('body').classList.remove('is-preload');
+        document.querySelector('.preloader').style.display = 'none';
     }, 100);
 }
