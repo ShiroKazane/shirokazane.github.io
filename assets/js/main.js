3cd7c16398f0d56ac5284b0d75ef8446f9e678db
ShiroKazane <bayuofficial773@gmail.com>
2022-09-03 09:08:23 +0700

    modified:   assets/css/main.css 	new file:   assets/fonts/FontAwesome.otf 	new file:   assets/fonts/fontawesome-webfont.eot 	new file:   assets/fonts/fontawesome-webfont.svg 	new file:   assets/fonts/fontawesome-webfont.ttf 	new file:   assets/fonts/fontawesome-webfont.woff 	new file:   assets/fonts/fontawesome-webfont.woff2 	new file:   assets/fonts/poppins-v20-latin-700.eot 	new file:   assets/fonts/poppins-v20-latin-700.svg 	new file:   assets/fonts/poppins-v20-latin-700.ttf 	new file:   assets/fonts/poppins-v20-latin-700.woff 	new file:   assets/fonts/poppins-v20-latin-700.woff2 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
-let menuToggle = document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation');
-let sec = document.querySelector('.sec');
+document.querySelector('.toggle').onclick = function() {
+    document.querySelector('.toggle').classList.toggle('active')
+    document.querySelector('.navigation').classList.toggle('active')
+    document.querySelector('.sec').classList.toggle('active')
+}
 
-menuToggle.onclick = function() {
-    menuToggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-    sec.classList.toggle('active')
+if (localStorage.getItem('theme')) {
+    document.querySelector('body').classList.toggle(localStorage.getItem('theme'))
+}
+
+document.querySelector('.themeSwitch').onclick = function() {
+    document.querySelector('body').classList.toggle('light')
+    localStorage.setItem('theme', document.querySelector('body').classList.value)
 }
