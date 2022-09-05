f3cf122f73bb621c4efc08df2a0d96684396b8d7
ShiroKazane <bayuofficial773@gmail.com>
2022-09-05 13:54:25 +0700

    modified:   assets/js/preloader.js

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,9 +1,5 @@
 window.onload = function () {
     window.setTimeout(function() {
-        document.querySelector('body').classList.remove('is-preload');
-    }, 100);
-
-    window.setTimeout(function() {
         document.querySelector('.preloader').style.display = 'none';
     }, 800);
 }
