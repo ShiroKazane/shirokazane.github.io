8b09363cee7af128995a0a1d9d3c3bdacd44c10c
ShiroKazane <bayuofficial773@gmail.com>
2022-09-05 13:49:42 +0700

    modified:   assets/css/main.css 	deleted:    assets/css/noscript.css 	modified:   assets/js/preloader.js 	modified:   index.html

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,5 +1,9 @@
 window.onload = function () {
-    window.setTimeout(function () {
+    window.setTimeout(function() {
         document.querySelector('body').classList.remove('is-preload');
     }, 100);
+
+    window.setTimeout(function() {
+        document.querySelector('.preloader').style.display = 'none';
+    }, 300);
 }
