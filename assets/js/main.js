e16165288a4ccc79015c99f43bb0c31d4d82e8ad
Shiro <bayuofficial773@gmail.com>
2024-03-20 00:39:44 +0700

    Fix some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-document.querySelector('.toggle').onclick = function() {
+document.querySelector('.toggle').onclick = function () {
+    document.querySelector('body').classList.toggle('active')
     document.querySelector('.toggle').classList.toggle('active')
     document.querySelector('.navigation').classList.toggle('active')
     document.querySelector('.sec').classList.toggle('active')
@@ -8,7 +9,7 @@ if (localStorage.getItem('theme')) {
     document.querySelector('body').classList.toggle(localStorage.getItem('theme'))
 }
 
-document.querySelector('.themeSwitch').onclick = function() {
+document.querySelector('.themeSwitch').onclick = function () {
     document.querySelector('body').classList.toggle('light')
     localStorage.setItem('theme', document.querySelector('body').classList.value)
 }
