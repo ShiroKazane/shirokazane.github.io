16082abf006c2ab036566582e6c42c61cc4dbdbd
Shiro <bayuofficial773@gmail.com>
2024-03-20 10:52:25 +0700

    class thing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ document.querySelector('.toggle').onclick = function () {
     document.querySelector('.toggle').classList.toggle('active')
     document.querySelector('.navigation').classList.toggle('active')
     document.querySelector('.sec').classList.toggle('active')
+    if (!document.querySelector('body').classList.length) {
+        document.querySelector('body').removeAttribute('class');
+    }
 }
 
 if (localStorage.getItem('theme')) {
@@ -12,4 +15,7 @@ if (localStorage.getItem('theme')) {
 document.querySelector('.themeSwitch').onclick = function () {
     document.querySelector('body').classList.toggle('light')
     localStorage.setItem('theme', document.querySelector('body').classList.value)
+    if (!document.querySelector('body').classList.length) {
+        document.querySelector('body').removeAttribute('class');
+    }
 }
--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,5 +1,11 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 window.onload = function () {
-    window.setTimeout(function() {
+    document.querySelector('body').style.overflowY = 'hidden';
+    window.setTimeout(function () {
         document.querySelector('.preloader').style.display = 'none';
+        document.querySelector('body').removeAttribute('style');
     }, 500);
 }
